0d37a1e56d7334678b168ac72039a5d0b3f9ced8
Gryshyn Vitaliy <vitaliy.gryshyn@extern.a1.at>
2020-12-14 12:52:04 +0200

    Song implementing

--- src/component/Welcome.js
+++ src/component/Welcome.js
@@ -27,7 +27,7 @@ export const SONG_MAIN_ENDPOINT = 'SONG_MAIN_ENDPOINT';
 export default class Welcome extends Component {
     componentDidMount() {
       //  localStorage.setItem("fullHouseHost", "http://localhost:1001/reperman/")
-        localStorage.setItem("fullHouseHost", "http://reperman.herokuapp.com/reperman/")
+        localStorage.setItem("fullHouseHost", "https://reperman.herokuapp.com/reperman/")
     }
 
     render() {
